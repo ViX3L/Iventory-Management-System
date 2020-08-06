fdefdf2066acffd24c835102afc69d6e7c0b5086
ViX3L <56755766+ViX3L@users.noreply.github.com>
2020-08-06 10:21:24 +0800

    Login Page created with database

--- Assignment - Inventory/Admin Panel.cs
+++ Assignment - Inventory/Admin Panel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Assignment___Inventory
 {
@@ -17,9 +18,25 @@ namespace Assignment___Inventory
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void loginButton_Click(object sender, EventArgs e)
         {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\asadu\Documents\GitHub\Assignment---Inventory\Assignment - Inventory\Database\LoginDB\LoginDB.mdf;Integrated Security=True;Connect Timeout=30");
+            string query = "Select * from LoginTable Where username = '" + usernameTextbox.Text.Trim() + "' and password = '" + passwordTextbox.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
 
+            if (dtbl.Rows.Count == 1)
+            {
+                Catalog objCatalog = new Catalog();
+                this.Hide();
+                objCatalog.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Access Denied, check username/password and try again!");
+            }
         }
 
         private void usernameTextbox_TextChanged(object sender, EventArgs e)
